460bc0e8c75ed20db52627e701d03f1800db9772
Anders Hovmöller <boxed@killingar.net>
2011-12-19 22:14:46 +0100

    metro-style implemented, lots of bugfixes, removed django from repo

---
+++ web_frontend/CMi/engine.py
@@ -0,0 +1,119 @@
+import os
+import re
+from os.path import join, getsize, splitext
+from CMi.directories import downloads_dir
+import datetime
+
+SUPPORTED_FILE_FORMATS = set([
+                              '.avi',
+                              '.mkv',
+                              '.m4v',
+                              '.mov',
+                              '.mp4',
+                              ])
+
+# TV Shows
+season_ep_regexs = [
+                    '(?P<name>.*)s(?P<season>\d\d)e(?P<episode>\d\d?)',
+                    '(?P<name>.*[^0-9])(?P<season>\d\d?)x(?P<episode>\d\d?)',
+                    '(?P<name>.*[^0-9])(?P<season>\d\d?)(?P<episode>\d\d)',
+                    ]
+date_regexs = [
+               '(?P<name>.*)(?P<year>\d\d\d\d) (?P<month>\d\d) (?P<day>\d\d)'
+               ]
+
+# Movies
+year_regexs = [
+    '(?P<name>.*)(?P<year>19\d\d)',
+    '(?P<name>.*)(?P<year>20\d\d)',
+    '(?P<name>.*)(?P<year>21\d\d)', # Handle stuff until I'm dead at least :P
+
+     # Some pseudo-matches to handle common suffixes
+    '(?P<name>.*) dvdrip(?P<year>)',
+    '(?P<name>.*) bluray(?P<year>)',
+]
+
+def supported_extension(s):
+    extension = splitext(s)[1]
+    return extension in SUPPORTED_FILE_FORMATS
+
+def has_videos(directory):
+    for root, dirs, files in os.walk(directory):
+        for f in files:
+            if supported_extension(f):
+                return True
+        break
+    return False
+
+def find_videos():
+    videos = []
+    for root, dirs, files in os.walk(downloads_dir):
+        for f in files:
+            if supported_extension(f):
+                videos.append(f)
+        for d in dirs:
+            if d.lower() in ('movies', 'tv shows', 'incomplete'):
+                continue
+            if has_videos(os.path.join(root, d)):
+                videos.append(d)
+        break
+    print 'found', videos
+    return videos
+
+def playable_path(path):
+    if os.path.isdir(path):
+        for root, dirs, files in os.walk(path):
+            if 'sample' in root.lower():
+                continue
+            for f in files:
+                if 'sample' in f.lower():
+                    continue
+                extension = os.path.splitext(f)[1]
+                if extension in SUPPORTED_FILE_FORMATS:
+                    path = os.path.join(root, f)
+                    break
+    return path
+
+def refresh_web_gui():
+    import subprocess
+    subprocess.call(['open', 'CMiVideoPlayer://refresh'])
+    print 'refresh GUI command sent'
+
+def canonical_format(s):
+    s2 = re.sub(r'^\[[^\]]*\]', '', s)
+    s2 = re.sub(r'^\([^\)]*\)', '', s2)
+    s2 = s2.lower().replace('.', ' ').replace('_', ' ').replace('-', ' ')
+    s2 = s2.replace('&', 'and').replace("'", '').replace(' 720p ', ' ').replace(' 1080p ', ' ').replace(' x264 ', ' ')
+    s2 = s2.replace('(', ' ').replace(')', ' ').replace('[', ' ').replace(']', ' ')
+    s2 = re.sub(r' +', ' ', s2).strip()
+    return s2
+
+def match_file(filename):
+    global season_ep_regexs
+    global date_regexs
+    global year_regexs
+    video = canonical_format(filename)
+    for date_regex in date_regexs:
+        m = re.match(date_regex, video)
+        if m:
+            break
+    if m:
+        name, aired = canonical_format(m.groupdict()['name']), datetime.datetime(int(m.groupdict()['year']), int(m.groupdict()['month']), int(m.groupdict()['day']))
+        return 'tv show', filename, name, aired
+
+    for year_regex in year_regexs:
+        m = re.match(year_regex, video.lower())
+        if m:
+            break
+    if m:
+        return 'movie', filename, canonical_format(m.groupdict()['name']), int(m.groupdict()['year'] or 0)
+
+    for season_ep_regex in season_ep_regexs:
+        m = re.match(season_ep_regex, video.lower())
+        if m:
+            break
+    if m:
+        name, season, episode = canonical_format(m.groupdict()['name']), m.groupdict()['season'], m.groupdict()['episode']
+        return 'tv show', filename, name, (season, episode)
+    print filename, 'matched nothing, ignoring...'
+    return None
---
+++ web_frontend/CMi/tests.py
@@ -0,0 +1,63 @@
+from CMi.engine import *
+
+s = """
+House.S07E23.Moving.On.HDTV.XviD-2HD.avi
+[PDTV-Xvid-Mp3-ITA] Dexter S05E11 [CR-Bt].avi
+Glee.S02E22.HDTV.XviD-LOL.[VTV].avi
+The.Colbert.Report.2011.05.31.James.B.Stewart.HDTV.XviD-FQM.[VTV].avi
+The.Daily.Show.2011.05.31.Jimmy.Fallon.HDTV.XviD-FQM.[VTV].avi
+(download at superseeds.org) Penn And Teller Fool Us S01E05 WS PDTV XviD-SuperS
+(download at superseeds.org) Penn.And.Teller.Fool.Us.S01E07.WS.PDTV.XviD-SuperS
+[ www.Speed.Cd ] - Penn.And.Teller.Fool.Us.S01E08.720p.HDTV.x264-ANGELiC
+[ www.TorrentDay.com ] - Penn.And.Teller.Fool.Us.S01E04.HDTV.XviD-COHD
+[ www.TorrentDay.com ] - Penn.And.Teller.Fool.Us.S01E06.HDTV.XviD-BARGE
+[ www.TorrentDay.com ] - So.You.Think.You.Can.Dance.S08E18.Top.8.Perform.HDTV.XviD-FQM
+[ www.TorrentDay.com ] - So.You.Think.You.Can.Dance.S08E19.2.of.8.Voted.Off.HDTV.XviD-FQM
+Bill.Bailey.Bewilderness.DVDRip.XViD
+Bill.Bailey.Live.Cosmic.Jam.1996.DVDRip.XviD-VoMiT
+Brotherhood.of.the.Rose.1989.Eng.DVDRip.XVID
+Crazy.Stupid.Love.DVDRip.XviD-TWiZTED
+Friends with Benefits 2011 R5 LiNE READNFO XViD - IMAGiNE
+In Time 2011 TS XviD READNFO - MiSTERE
+So.You.Think.You.Can.Dance.S08E20.Top.6.Perform.HDTV.XviD-FQM.avi
+X-Men First Class 2011 R5 LiNE READNFO XViD - IMAGiNE
+Your Highness (2011) DVDRip XviD-MAXSPEED
+White Collar S02E14 Payback HDTV XviD-FQM
+White.Collar.S02E10.Burkes.Seven.HDTV.XviD-FQM.avi
+Boston Legal - 420 - Patriot Acts.avi
+Boston Legal - [S05E01] - Smoke Signals.avi
+Boston Legal - 05x02 - Guardians and Gatekeepers.avi
+Boston Legal - 5x2 - Dances With Wolves.avi
+Boston Legal - (S05E04) - True Love.avi
+Glee S03E06 Mash Off HDTV XviD-LOL [eztv]
+Glee S03E06 Mash Off HDTV XviD-LOL [eztv].avi
+Greys Anatomy S08E08 Heart-Shaped Box HDTV.XviD-LOL.[VTV].avi
+House S08E05 The Confession HDTV XviD-ASAP [eztv]
+House S08E06 Parents HDTV XviD-2HD [eztv]
+House.S08E03.HDTV.XviD-LOL.avi
+House.S08E04.HDTV.XviD-LOL.avi
+30.Minutes.Or.Less.2011.BDRip-HDT.avi
+Iron Man 2008.720p BluRay DTS x264-ESiR
+Iron.Man.2.2010.720p.BluRay.x264-WiKi
+Midnight.in.Paris.DVDRip.XviD-TARGET
+Water for Elephants (2011) DVDRip XviD-MAXSPEED
+Where.The.Wild.Things.Are.720p.Bluray.x264-HUBRIS
+X-Men The Last Stand
+1000.Ways.To.Die.S05E22.HDTV.XviD-aAF
+90210.S04E10.480p.WEB-DL.x264-mSD
+The X Files-I Want To Believe[2008]DvDrip-aXXo
+X-Files 1x24 - The Erlenmeyer Flask.avi
+The.Colbert.Report.2011.06.07.Sugar.Ray.Leonard.HDTV.XviD-FQM.[VTV].avi
+(download at superseeds.org) Penn.And.Teller.Fool.Us.S01E07.WS.PDTV.XviD-SuperS
+[ www.Speed.Cd ] - Penn.And.Teller.Fool.Us.S01E08.720p.HDTV.x264-ANGELiC
+"""
+
+def classify(s):
+    r = match_file(s)
+    if r:
+        print '# %s\t\t\t"%s"' % (s, r[0]), r[1:]
+    else:
+        print '$', s
+
+for line in s.strip().split('\n'):
+    classify(line)
