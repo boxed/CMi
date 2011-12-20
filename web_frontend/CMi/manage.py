85a4e9d11b21c03697fcb04386ea3ce08e01f55d
Anders Hovmöller <anders.hovmoller@trioptima.com>
2011-12-20 09:11:56 +0100

    added missing cmi.db file and fixed path in manage.py

--- web_frontend/CMi/manage.py
+++ web_frontend/CMi/manage.py
@@ -2,8 +2,8 @@
 import os
 import sys
 sys.path.append(os.getcwd())
-sys.path.append(os.path.join(os.path.split(os.getcwd())[0], 'django')) # for embedded use
-sys.path.append(os.path.join(os.path.split(os.getcwd())[0], 'django/django')) # for development
+sys.path.append(os.path.split(os.getcwd())[0]) # for embedded use
+sys.path.append(os.path.join(os.path.split(os.getcwd())[0], 'django')) # for development
 
 from django.core.management import execute_manager
 try:
