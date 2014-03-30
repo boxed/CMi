0e75ab63458ebe4b8d185e443d31e6188cff9527
Anders Hovmöller <boxed@killingar.net>
2014-03-30 13:37:09 +0200

    Fixed incorrect timezone usage

--- web_frontend/CMi/settings.py
+++ web_frontend/CMi/settings.py
@@ -43,7 +43,7 @@ DATABASES = {
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = None
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
