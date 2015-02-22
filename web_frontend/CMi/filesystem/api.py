e02c0b32244d9134aa8c7d3e4dada35855abe408
Anders Hovmöller <boxed@killingar.net>
2015-02-22 15:44:58 +0100

    Minor cosmetic fixes

--- web_frontend/CMi/filesystem/api.py
+++ web_frontend/CMi/filesystem/api.py
@@ -1,13 +1,13 @@
 from django.template.loader import render_to_string
 from django.conf.urls import patterns
 
+
 def tiles():
     return [
         (100, render_to_string('tile.html', {
             'url': '/browse/',
             'image': '/site-media/folders.svg',
-            'title': 'Browse files',
-            'content': ''
+            'title': 'Browse Files',
         })),
     ]
 
